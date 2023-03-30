c443bafcf217e3633fc58b48d7454abd0be35672
Harsh <73978943+Harsh786786@users.noreply.github.com>
2023-03-30 10:43:40 +0530

    service account commit

---
+++ Google/auth_with_service_account/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.59.0"
+    }
+  }
+} 
+
+provider "google" {
+  # Configuration options
+  project = "linen-arch-374006"
+  region = "us-central1"
+  zone = "us-central1-a"
+  credentials = "keys.json"
+}
+
+resource google_storage_bucket "GCS786rojharsh" {
+  name = "harshbucketharsh786777999"
+  location      = "US"
+}
